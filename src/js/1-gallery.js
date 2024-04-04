26843490f89fba698de646d24e41b6ef8d108946
NazarKapitonov <155971258+NazarKapitonov@users.noreply.github.com>
2024-04-04 16:19:10 +0300

    lightbox install

---
+++ src/js/1-gallery.js
@@ -0,0 +1,9 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
